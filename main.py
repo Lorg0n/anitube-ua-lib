74e00d08072ca48a80a447b50c3ba0cb2f84becf
Lorg0n <lorgon.kv@gmail.com>
2023-07-27 01:59:52 +0300

    Change examples in README.md

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from anitube import AniTube
 
 api = AniTube()
 
-animes = api.search_anime(search="Демон", limit=100)
+animes = api.search_anime(search="Демон", limit=5)
 for anime in animes:
     print(anime.name)
